1d7bbfe977d2cd72e7311cef342783eaf74b5c61
aidancrowl <43039785+aidancrowl@users.noreply.github.com>
2018-10-15 09:44:44 -0400

    Add files via upload

---
+++ IRServo.ino
@@ -0,0 +1,37 @@
+#include <IRremote.h>
+#include <IRremoteInt.h>
+
+//#include <Servo.h>
+
+
+int RECV_PIN = 4;
+//Servo servo;
+
+IRrecv irrecv(RECV_PIN);
+
+decode_results results;
+
+void setup()
+{
+  Serial.begin(9600);
+  irrecv.enableIRIn(); // Start the receiver
+  pinMode(8, OUTPUT);
+  //servo.attach(0);
+  //servo.write(180);
+}
+
+void loop() {
+  if (irrecv.decode(&results)) {
+    Serial.println(results.value);
+    irrecv.resume(); // Receive the next value
+  }
+  if (results.value == 50165895) {
+    digitalWrite(8, HIGH);
+    //servo.write(180);
+  }
+  else if (results.value == 50157735) {
+    digitalWrite(8, LOW);
+    //servo.write(0);
+  }
+ }
+
