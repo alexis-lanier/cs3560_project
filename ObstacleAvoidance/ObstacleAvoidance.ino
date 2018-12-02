d317535e6577ab5e0be340b125b8ad582cf3d720
Joseph-Ricker <43178617+Joseph-Ricker@users.noreply.github.com>
2018-12-02 18:08:19 -0500

    ObstacleAvoidance.ino
    Updated Obstacle Avoidance file

--- ObstacleAvoidance/ObstacleAvoidance.ino
+++ ObstacleAvoidance/ObstacleAvoidance.ino
@@ -1,10 +1,3 @@
-/**
- * @file ObstacleAvoidance.ino
- * @brief controls servos and ultrasonic sensor to turn when detects an obstacle
- * @author Aidan Crowl/Alexis Lanier/Joey Ricker
- */
-
-
 #include <Servo.h>
 
 const int pingPin = 7;
@@ -13,16 +6,18 @@ Servo servoRight;
 Servo servoLeft;
 
 void setup() {
+  
   // initialize serial communication:
   Serial.begin(19200);
   servoRight.attach(A3);
   servoLeft.attach(A4);
+  
 }
 
 void loop() {
 
-  servoRight.write(180);
-  servoLeft.write(0);
+  servoRight.write(0);
+  servoLeft.write(180);
   
   double duration, inches, cm;
   
@@ -46,19 +41,19 @@ void loop() {
   Serial.print("cm");
   Serial.println();
 
-/*
-  if(inches < 2) {
-      servoRight.write(180);
-      delay(2000);
+
+  if(inches < 4) {
+      servoLeft.write(0);
+      delay(500);
   }
   else {
-    servoRight.write(0);
+    servoLeft.write(180);
   }
-*/
-    
+
+/*    
   while(inches < 2) {
 
-      servoRight.write(0);
+      servoRight.write(180);
 
       pinMode(pingPin, OUTPUT);
       digitalWrite(pingPin, LOW);
@@ -80,25 +75,15 @@ void loop() {
 
       delay(100);
   }
-
+*/
   delay(100);
 }
 
-/**
-*@brief converts microsecond duration to a distance in inches
-*@param duration of type double
-*@return duration in inches
-*/
 double microsecondsToInches(double duration) {
  
   return duration / 73.746 / 2.0;
 }
 
-/**
-*@brief converts microsecond duration to a distance in cm
-*@param duration of type double
-*@return duration in cm
-*/
 double microsecondsToCentimeters(double microseconds) {
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
   // The ping travels out and back, so to find the distance of the
